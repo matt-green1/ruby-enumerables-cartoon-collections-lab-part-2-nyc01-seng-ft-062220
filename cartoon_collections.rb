58b79a0226966363e946064258a93b15c6dc6bd3
Matt Green <magreen118@gmail.com>
2020-06-05 22:45:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,6 +6,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
+  array.map {|element| element.}
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
 end
